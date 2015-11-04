4fe3a6d5c4aed2d9217262388111275b7f602b3b
Patrick Trompeter <patrick.trompeter@gmail.com>
2015-11-04 13:29:34 -0800

    updated rockpaperscissors.js with getwinner function

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,14 +25,14 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return (move || getInput());
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return (move || randomPlay());
 }
 
 function getWinner(playerMove,computerMove) {
@@ -41,6 +41,13 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+    if (playerMove === computerMove) {
+        winner = 'tie';
+    } else if (((playerMove === 'rock') && (computerMove === 'scissors')) || ((playerMove === 'scissors') && (computerMove === 'paper')) || ((playerMove === 'paper') && (computerMove === 'rock'))) {
+        winner = 'player';
+    } else {
+        winner = 'computer'
+    }
     return winner;
 }
 
